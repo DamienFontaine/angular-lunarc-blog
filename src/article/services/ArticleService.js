a15cc0828351edc943b95b9a58539c28a0cf33f8
Damien Fontaine <damien.fontaine@lineolia.net>
2016-01-20 16:07:19 +0100

    Add GetByPretty function to Article Service

--- src/article/services/ArticleService.js
+++ src/article/services/ArticleService.js
@@ -7,10 +7,14 @@
  * Service d'accès aux articles
  */
 angular.module('lunarc.article').factory("ArticleService", ['$resource', function($resource){
-  return $resource('/article/:Id', {Id:'@ID'},
+  return $resource('/article/:Id', {Id:'@ID', Pretty: '@Pretty'},
   {
     update: { method: 'PUT', params: {
       Id: '@ID'
-    }}
+    }},
+    getByPretty: {
+      method: 'GET',
+      url: '/article/:Pretty'
+    }
   });
 }]);
--- test/unit/article/services/ArticleService.test.js
+++ test/unit/article/services/ArticleService.test.js
@@ -10,11 +10,11 @@ describe('lunarc.article.ArticleService', function() {
   }));
 
   afterEach(function() {
-     $httpBackend.verifyNoOutstandingExpectation();
-     $httpBackend.verifyNoOutstandingRequest();
-   });
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
 
-  it('should return an article', function() {
+  it('should return an article by Id', function() {
     var url = '/article/564a26eb1d41c81764000001';
     var response = {
       "Id": "564a26eb1d41c81764000001",
@@ -43,6 +43,40 @@ describe('lunarc.article.ArticleService', function() {
     $httpBackend.expectGET(url);
     var article = ArticleService.get({
       Id: '564a26eb1d41c81764000001'
+    });
+    $httpBackend.flush();
+    expect(article.Id).toBe(response.Id);
+  });
+
+  it('should return an article by Titre', function() {
+    var url = '/article/new-article';
+    var response = {
+      "Id": "564a26eb1d41c81764000001",
+      "Titre": "New Article",
+      "Pretty": "new-article",
+      "Texte": "Content",
+      "Tags": [],
+      "Image": {
+        "Url": "",
+        "Credit": ""
+      },
+      "Vignette": {
+        "Url": "",
+        "Credit": ""
+      },
+      "Status": "PENDING",
+      "Create": "2015-11-16T19:56:43.614+01:00",
+      "Modified": "2015-11-16T20:03:14.178+01:00",
+      "UserRef": {
+        "Collection": "user",
+        "ID": "55f06ff71d41c85de4000001",
+        "Database": ""
+      }
+    };
+    $httpBackend.when('GET', url).respond(response);
+    $httpBackend.expectGET(url);
+    var article = ArticleService.getByPretty({
+      Pretty: 'new-article'
     });
     $httpBackend.flush();
     expect(article.Id).toBe(response.Id);
